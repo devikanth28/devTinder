41211d52432e148eaf2523ade928319a237ee69f
devikanth28 <devikanth28@gmail.com>
2025-02-03 11:30:24 +0530

    explore the get, post methods

--- src/index.js
+++ src/index.js
@@ -3,9 +3,34 @@ const express = require("express");
 
 const app = express();
 
+//while writing the routes is matter of sequence(order)
+
+app.get("/user", (req, res)=>{
+    res.send({firstName:"Devi", lastName:"Kanth"})
+});
+
+
+app.post("/user", (req, res)=>{
+    console.log("Save The Data Successfully");
+    res.send("Data Saved")
+})
+
+
+
+//this will match all the HTTP method API calls to test
 app.use("/test", (req, res)=>{
     res.send(" Namaste Devikanth")
-})
+});
+
+// app.use("/hello/2", (req, res)=>{
+//     res.send("hello/2")
+// });
+
+// app.use("/hello", (req, res)=>{
+//     res.send("Devikanthhh");
+// });
+
+
 
 app.listen(7777, ()=>{
     console.log("server successfully listening on port 3003")
