f353bd097c72e5c992aa2b99da41de7d756cd250
devikanth28 <devikanth28@gmail.com>
2025-02-07 15:36:24 +0530

    schema validations

--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,50 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true,
+        minLength:4,
+        maxLength:100
     },
     lastName:{
-        type:String
+        type:String,
+        required:true,
     },
     emailId:{
-        type:String
+        type:String,
+        required:true,
+        unique:true,
     },
     password:{
-        type:String
+        type:String,
+        required:true,
     },
     age:{
-        type:String
+        type:Number,
+        min:18,
+        max:50
+        
     },
     gender:{
-        type:String
-    }
+        type:String,
+        // validate(value){
+        //     if(["male", "female", "others"].includes(value)){
+        //         throw new Error("Gender data is not valid")
+        //     }
+        // }
+    },
+    photoUrl:{
+        type:String,
+        default:"https://www.cgg.gov.in/wp-content/uploads/2017/10/dummy-profile-pic-male1.jpg",
+    },
+    about:{
+        type:String,
+        default:"this is the default about of the user",
+    },
+    skills:{
+        type:[String]
+    },
+    timestamps:true,
 });
 
 // const userModel = 
